12855d3036ab3516c602c7336cd5c3098d685520
SpectreSpect <spectrefromthefuture@gmail.com>
2022-04-28 21:11:40 +0400

    Reworked main page

--- index.php
+++ index.php
@@ -4,24 +4,23 @@
     <meta charset = "UTF-8">
     <title>My site</title>
     <link rel = "stylesheet" href = "Styles/header.css">
+    <link rel = "stylesheet" href = "Styles/main.css">
+    <link rel = "stylesheet" href = "Styles/text_styles.css">
 </head>
 <body>
-    <?php
-    include('header.php');
-    ?>
-    <div class = "background">
-        <img src = "images/background.jpg" alt = "Background">
-    </div>
-    <div class = "welcome">
-        <h1>Добро пожаловать</h1><br>
-    </div>
-    <div class = "welcome">
-        Some text text text text text text text text text text text text text text text<br>
-        text text text text text text text text text text text text text text text text.<br>
-        Some text text text text text text text text text text text text text text text<br>
-        text text text text text text text text text text text text text text text text.<br>
-        Some text text text text text text text text text text text text text text text<br>
-        text text text text text text text text text text text text text text text text.<br>
+    <?php include('header.php'); ?>
+
+    <div class = "introTextAndImage">
+        <div class = "background">
+            <img src = "images/lake_and_mountains.jpg" alt = "Background">
+        </div>
+        <div class = "mobile">
+            <div class = "heroItem">
+                <h1 class = "c-heading-3">Добро пожаловать</h1>
+                <p class = "c-paragraph-2">Microsoft Edge is the fast and secure browser that helps you protect your data and save time and money. </p>
+                <a class = "see_projects" href="index.php">Посмотреть проекты</a>
+            </div>
+        </div>
     </div>
 </body>
 </html>
--- projects.php
+++ projects.php
@@ -4,11 +4,14 @@
     <meta charset = "UTF-8">
     <title>My site</title>
     <link rel = "stylesheet" href = "Styles/header.css">
+    <link rel = "stylesheet" href = "Styles/projects.css">
 </head>
 <body>
     <?php
     include('header.php');
     ?>
-    <h1>Проекты</h1>
+    <div class = "testDiv">
+        <p class = "testHeadingClass">Проекты</p>
+    </div>
 </body>
 </html>
